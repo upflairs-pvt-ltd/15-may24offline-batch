d14a38c5d7ee1672b379b2f0a634214d3218b050
Ranjit-singh-786 <jiradhey402@gmail.com>
2024-06-07 12:19:53 +0530

    7june

--- ML-Project/app.py
+++ ML-Project/app.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,url_for,request,jsonify
+import joblib 
+model = joblib.load('linear_regression_model.lb')
+
+
 
 app = Flask(__name__)
 
@@ -11,8 +15,21 @@ def user_data():
     return render_template('user_data.html')
 
 
+# POST 
+@app.route('/predict',methods=['GET','POST'])
+def predict():
+    if request.method == 'POST':
+        brand_name = int(request.form['brand_name'])
+        Kms_Driven = int(request.form['Kms_Driven'])  
+        owner = int(request.form['owner'])
+        age = int(request.form['age'] ) 
+        power = int(request.form['power'])  
+
+        user_data = [[owner,brand_name,Kms_Driven,age,power]]
+        pred = model.predict(user_data)[0][0] # [25141.231]
+        prediction = str(round(pred,2))
+        return render_template('user_data.html', prediction_text=prediction)
 
 if __name__ =="__main__":
     app.run(debug=True)
 
-## google form with in two 2
